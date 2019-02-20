b2b5df2c4d277b5041e75d766f08a7a198ae71b6
Unknown <azimov308@live.com>
2019-02-19 22:57:29 -0500

    fixed print line message

--- rpn.py
+++ rpn.py
@@ -1,15 +1,15 @@
 rpn = []
 userInput = 0
-con = 1
+cont = 1
 
-while(con == 1):
+while(cont == 1):
   userInput = raw_input()
   try:
     userInput = int(userInput)
     rpn.append(userInput)
   except ValueError:
     if not rpn:
-        print("invalid")
+        print("invalid operation")
     elif (len(rpn)>1 and userInput== "+"):
         var1 = rpn.pop()
         var2 = rpn.pop()
@@ -35,4 +35,4 @@ while(con == 1):
         rpn.append(var1*-1)
         print(int(var1*-1))
     elif (type(userInput) != int):
-            print("invalid")
+        print("invalid operation")
