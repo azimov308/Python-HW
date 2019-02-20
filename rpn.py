3fe8d5c7a491907202acd39f00b6ca7abb3e5b03
Unknown <azimov308@live.com>
2019-02-19 23:05:51 -0500

    formatted

--- rpn.py
+++ rpn.py
@@ -3,36 +3,36 @@ userInput = 0
 cont = 1
 
 while(cont == 1):
-  userInput = raw_input()
-  try:
-    userInput = int(userInput)
-    rpn.append(userInput)
-  except ValueError:
-    if not rpn:
-        print("invalid operation")
-    elif (len(rpn)>1 and userInput== "+"):
-        var1 = rpn.pop()
-        var2 = rpn.pop()
-        rpn.append(var1+var2)
-        print(int(var1+var2))
-    elif (len(rpn)>1 and userInput == "-"):
-        var1 = rpn.pop()
-        var2 = rpn.pop()
-        rpn.append(var2-var1)
-        print(int(var2-var1))
-    elif (len(rpn)>1 and userInput == "*"):
-        var1 = rpn.pop()
-        var2 = rpn.pop()
-        rpn.append(var1*var2)
-        print(int(var1*var2))
-    elif (len(rpn)>1 and userInput == "/"):
-        var1 = rpn.pop()
-        var2 = rpn.pop()
-        rpn.append(var1/var2)
-        print(int(var1/var2))
-    elif (userInput == "~"):
-        var1 = rpn.pop()
-        rpn.append(var1*-1)
-        print(int(var1*-1))
-    elif (type(userInput) != int):
-        print("invalid operation")
+    userInput = raw_input()
+    try:
+        userInput = int(userInput)
+        rpn.append(userInput)
+    except ValueError:
+        if not rpn:
+            print("invalid operation")
+        elif (len(rpn) > 1 and userInput == "+"):
+            var1 = rpn.pop()
+            var2 = rpn.pop()
+            rpn.append(var1+var2)
+            print(int(var1+var2))
+        elif (len(rpn) > 1 and userInput == "-"):
+            var1 = rpn.pop()
+            var2 = rpn.pop()
+            rpn.append(var2-var1)
+            print(int(var2-var1))
+        elif (len(rpn) > 1 and userInput == "*"):
+            var1 = rpn.pop()
+            var2 = rpn.pop()
+            rpn.append(var1*var2)
+            print(int(var1*var2))
+        elif (len(rpn) > 1 and userInput == "/"):
+            var1 = rpn.pop()
+            var2 = rpn.pop()
+            rpn.append(var1/var2)
+            print(int(var1/var2))
+        elif (userInput == "~"):
+            var1 = rpn.pop()
+            rpn.append(var1*-1)
+            print(int(var1*-1))
+        elif (type(userInput) != int):
+            print("invalid operation")
